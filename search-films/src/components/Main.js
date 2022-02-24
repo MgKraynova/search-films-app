edfed6cc1167a5bcbe6e9c493e2b2759a9ae213a
Marina Kraynova <kraynovamg@gmail.com>
2022-02-24 18:57:38 +0300

    add loading

--- search-films/src/components/Main.js
+++ search-films/src/components/Main.js
@@ -15,7 +15,7 @@ function Main() {
 
     let navigate = useNavigate();
 
-    // const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
 
     function handleApiError(err) {
@@ -24,6 +24,8 @@ function Main() {
 
     function search(e) {
         e.preventDefault();
+
+        setIsLoading(true);
 
         return fetch(`http://www.omdbapi.com/?t=${inputValue}&plot=full&apikey=9b9c63d2`)
             .then((res) => {
@@ -47,23 +49,24 @@ function Main() {
             .catch((err) => {
                 handleApiError(err);
                 navigate("/error");
-            });
-        // .finally(() => {
-        //     setIsLoading(false);
-        // }); //todo добавить лоадер
+            })
+        .finally(() => {
+            setIsLoading(false);
+        });
     }
 
     return (
         <main className="main">
             <SearchForm handleChange={setInputValue} handleSubmit={search}/>
             <div className="main__content">
+                {isLoading ? <div className="loader">
+                </div> : ''}
                 <Routes>
                     <Route exact path="/" element={null} />
                     <Route path="/card" element={<Card data={data}/>}/>
                     <Route path="/info" element={<Info data={data}/>}/>
                     <Route path="not-found" element={<NotFound/>} />
                     <Route path="*" element={<Error />} />
-                    {/*//todo сделать роут "инфа не найдена"*/}
                 </Routes>
             </div>
         </main>

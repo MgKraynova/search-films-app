d025e40fafe6c9f2ea190ccd4be1f21b29695eab
Marina Kraynova <kraynovamg@gmail.com>
2022-04-19 10:20:19 +0300

    fix bugs

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,5 @@
 import Card from "./Card";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 
 function Cards({cards, showCardDetails}) {
